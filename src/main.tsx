8574bad2046352bec7a3ed1a1d54303ae5a11063
jesper <jesper@tarnvik.com>
2025-01-26 08:41:04 +0100

    Flowbite removal. Added navbar.

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import 'flowbite/dist/flowbite.min.js';
-import {DarkThemeToggle, Flowbite} from "flowbite-react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Flowbite>
       <App />
-      <DarkThemeToggle />
-    </Flowbite>
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
-import flowbite from "flowbite-react/tailwind";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    flowbite.content(),
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
   ],
 }
