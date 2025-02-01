72a3d3652fba8bb808f01f344865b753466185ae
jesper <jesper@tarnvik.com>
2025-02-01 15:52:45 +0100

    Icon test

--- src/components/common/line/index.tsx
+++ src/components/common/line/index.tsx
@@ -1,3 +1,6 @@
+import { TbBus } from "react-icons/tb";
+import { PiBus } from "react-icons/pi";
+
 type Props = {
   line: Line
 }
@@ -5,6 +8,8 @@ type Props = {
 export function Line({line}: Props) {
   return (
     <div className="flex">
+      <TbBus className="bg-black text-white mt-[4px]"/>&nbsp;
+      <PiBus className="bg-black text-white mt-[4px]"/>&nbsp;
       <div
         className="px-[3px] grid-line font-signage bg-black text-white font-extrabold pt-[2px] mb-[4px] leading-[calc(1em-4px)] mt-[4px]">
         {line.designation}
