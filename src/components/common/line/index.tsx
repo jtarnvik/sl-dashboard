1803dad7c477aaa5f648a03aa119e31d652aeaad
jesper <jesper@tarnvik.com>
2025-02-01 15:55:45 +0100

    Icon test

--- src/components/common/line/index.tsx
+++ src/components/common/line/index.tsx
@@ -8,8 +8,8 @@ type Props = {
 export function Line({line}: Props) {
   return (
     <div className="flex">
-      <TbBus className="bg-black text-white mt-[4px]"/>&nbsp;
-      <PiBus className="bg-black text-white mt-[4px]"/>&nbsp;
+      <TbBus className=" mt-[4px]"/>&nbsp;
+      <PiBus className=" mt-[4px]"/>&nbsp;
       <div
         className="px-[3px] grid-line font-signage bg-black text-white font-extrabold pt-[2px] mb-[4px] leading-[calc(1em-4px)] mt-[4px]">
         {line.designation}
