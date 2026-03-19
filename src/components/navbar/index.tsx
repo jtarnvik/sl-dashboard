920920630c40b77f30d7df21d340d2e7684dbb69
jesper <jesper@tarnvik.com>
2026-03-19 17:36:23 +0100

    Login button added

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,5 @@
+import {SLButton} from "../common/sl-button";
+import {useUser, useUserLoginState, UserLoginState} from "../../hook/use-user";
 import {SlLogo} from "./sl-logo";
 
 type Props = {
@@ -5,6 +7,20 @@ type Props = {
 }
 
 export function Navbar({heading}: Props) {
+  const loginState = useUserLoginState();
+  const {login, logout} = useUser();
+
+  const isLoggedIn = loginState === UserLoginState.LoggedIn;
+  const isLoading = loginState === UserLoginState.Loading;
+
+  function handleAuthButton() {
+    if (isLoggedIn) {
+      logout();
+    } else {
+      login();
+    }
+  }
+
   return (
     <nav
       className="bg-[#2870f0] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
@@ -15,6 +31,9 @@ export function Navbar({heading}: Props) {
           {heading}
           </span>
         </div>
+        <SLButton onClick={handleAuthButton} disabled={isLoading}>
+          {isLoggedIn ? "Logga ut" : "Logga in"}
+        </SLButton>
       </div>
     </nav>
   );
